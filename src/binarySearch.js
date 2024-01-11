4f5c72dc9f5e0c5a379030df3cc2d52ffed199bb
Nicolas Wanner <your@example.com>
2024-01-11 11:29:42 +0100

    ok

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -6,26 +6,30 @@ function binarySearch(hayStack, needle, start = 0, end) {
   //  }
   //}
 
+  if(start > end) {
+    return false // would be error
+  }
+
   if(end === undefined) {
     end = hayStack.length
   }
 
-  const middleIndex = start + Math.floor(end / 2)
+  const middleIndex = Math.floor((start + end) / 2)
 
   if(hayStack[middleIndex] === needle) {
     return middleIndex
   }
 
   if(needle < hayStack[middleIndex]) {
-    console.log(middleIndex)
-    return binarySearch(hayStack, needle, middleIndex, middleIndex)
+    //console.log(middleIndex)
+    return binarySearch(hayStack, needle, start, middleIndex - 1)
   }
   if(needle > hayStack[middleIndex]) {
-    console.log(middleIndex)
-    return binarySearch(hayStack, needle, middleIndex, end)
+    //console.log(middleIndex)
+    return binarySearch(hayStack, needle, middleIndex + 1, end)
   }
 }
 
 const exampleArr = [1,2,3,4,5,6,7,8,9,10]
 
-console.log(binarySearch(exampleArr, 9))
+console.log(binarySearch(exampleArr, 11))
