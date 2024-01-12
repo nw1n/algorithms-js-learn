0c3a182d5b35cd811db49f63142f30a46d83f960
Nicolas Wanner <your@example.com>
2024-01-12 16:39:15 +0100

    ok

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,4 +1,4 @@
-function binarySearch(hayStack, needle, start = 0, end) {
+function binarySearchR(hayStack, needle, start = 0, end) {
   //for (let i = 0; i < hayStack.length; i++) {
   //  const item = hayStack[i]
   //  if(needle === item) {
@@ -22,14 +22,58 @@ function binarySearch(hayStack, needle, start = 0, end) {
 
   if(needle < hayStack[middleIndex]) {
     //console.log(middleIndex)
-    return binarySearch(hayStack, needle, start, middleIndex - 1)
+    return binarySearchR(hayStack, needle, start, middleIndex - 1)
   }
   if(needle > hayStack[middleIndex]) {
     //console.log(middleIndex)
-    return binarySearch(hayStack, needle, middleIndex + 1, end)
+    return binarySearchR(hayStack, needle, middleIndex + 1, end)
   }
 }
 
+
+
+
+
+
+
+
+
+
+function binarySearchLoop(hayStack, needle) {
+  let start = 0;
+  let end = hayStack.length
+  let middleIndex
+
+  while(true) {
+    middleIndex = Math.floor((end + start) / 2)
+    let middleItem = hayStack[middleIndex]
+
+    if(middleItem === needle) {
+      return middleIndex
+    }
+
+    if(needle < middleItem) {
+      end = middleIndex - 1
+    } else {
+      start = middleIndex + 1
+    }
+    console.log('middleIndex:', middleIndex);
+    console.log('start:', start);
+    console.log('end:', end);
+  }
+}
+
+
+
+
+
+
 const exampleArr = [1,2,3,4,5,6,7,8,9,10]
 
-console.log(binarySearch(exampleArr, 11))
+console.log('result')
+console.log(binarySearchLoop(exampleArr, 10))
+//console.log(binarySearch(exampleArr, 1))
+//console.log(binarySearch(exampleArr, 4))
+//console.log(binarySearch(exampleArr, 5))
+//console.log(binarySearch(exampleArr, 6))
+//console.log(binarySearch(exampleArr, 10))
