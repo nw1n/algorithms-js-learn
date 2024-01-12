7b8d40efea7ddf8a0ccb0df423b32dd622845bb9
Nicolas Wanner <your@example.com>
2024-01-12 22:55:07 +0100

    ok

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -2,13 +2,14 @@ function insertionSort(srcArr) {
   for(let i = 0; i < 99; i++) {
     for(let iOuter = 1; iOuter < srcArr.length; iOuter++) {
       const mainVal = srcArr[iOuter]
-      for(let iInner = 1; iInner < iOuter; iInner++) {
+      for(let iInner = 1; iInner <= iOuter; iInner++) {
         const leftNeighborI = iOuter - iInner
         const leftNeighborVal = srcArr[leftNeighborI]
         if(mainVal < leftNeighborVal) {
           for(let shiftI = leftNeighborI; shiftI >= 0; shiftI--) {
+            const temp = srcArr[shiftI + 1]
             srcArr[shiftI + 1] = srcArr[shiftI]
-            srcArr[shiftI] = mainVal
+            srcArr[shiftI] = temp
           }
         } else {
           break;
@@ -21,6 +22,6 @@ function insertionSort(srcArr) {
   return srcArr
 }
 
-const unsortedArr = [4,3,1,8,9,2,3,5,1,8,1,2]
+const unsortedArr = [5,4,3,2,1,7,8,9,6]
 
 console.log(insertionSort(unsortedArr))
