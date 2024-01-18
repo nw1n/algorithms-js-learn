d6a98a0c1a0bafac2796beffb0ab35b494555326
Nicolas Wanner <your@example.com>
2024-01-18 22:53:23 +0100

    ok

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
-// test.mjs
 import { bubbleSort2 } from '../src/bubbleSort.js'
 import { expect } from 'chai'
 
-// Mocha test suite
 describe('bubbleSort', () => {
-    // Mocha test case
     it('should sort array', () => {
         const src = [9, 8, 7, 1, 6, 5, 3, 2, 4]
         const aim = [1, 2, 3, 4, 5, 6, 7, 8, 9]
